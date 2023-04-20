cb7ebc424d7c389174b370679f154607b9eab756
brianmureithi <brianmurithi65@gmail.com>
2023-04-20 11:35:32 +0300

    Add next project files

---
+++ src/pages/events/event.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Event() {
+  return (
+    <h1 className='text-xl font-bold'>Event 1</h1>
+  )
+}
---
+++ src/pages/events/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function aboutUs() {
+  return (
+    <h1 className='text-xl font-bold'>Events</h1>
+  )
+}
